b23cc115bae7d2a1700bf319ea0098a3a1b72071
theodorross <theodor.a.ross@uit.no>
2025-02-26 13:24:39 +0100

    updating lumi files

--- gene_embedding/lumi_files/LUMI_batch_job.sh
+++ gene_embedding/lumi_files/LUMI_batch_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=lumi_files/logs/gene_embedding.o%j # Name of stdout output file
 #SBATCH --error=lumi_files/logs/gene_embedding.e%j  # Name of stderr error file
 #SBATCH --account=project_465001381
-#SBATCH --time=48:00:00
+#SBATCH --time=00:10:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
