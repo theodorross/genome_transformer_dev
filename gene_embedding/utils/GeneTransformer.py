11e5252a491668f9950dc0a35d19be05e72e3a23
theodorross <theodor.a.ross@uit.no>
2025-03-06 16:00:17 +0100

    updating LUMI files

--- gene_embedding/utils/GeneTransformer.py
+++ gene_embedding/utils/GeneTransformer.py
@@ -185,6 +185,7 @@ class GeneTransformer(models.Model):
         discard_options = cardinality % batch_size_options
         new_batch_size = batch_size_options[np.argmin(discard_options)]
         print("DEBUG:")
+        print("n_replicas:", n_replicas)
         print("batch_size_options:", batch_size_options)
         print("discard_options:", discard_options)
         print("new_batch_size:", new_batch_size)
