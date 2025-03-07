cb177387dabd939860ba8c79e840be393b4ed976
theodorross <theodor.a.ross@uit.no>
2025-03-07 13:10:55 +0100

    updating LUMI files

--- gene_embedding/lumi_files/LUMI_batch_job.sh
+++ gene_embedding/lumi_files/LUMI_batch_job.sh
@@ -6,8 +6,8 @@
 #SBATCH --time=00:10:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gpus-per-node=8
-#SBATCH --cpus-per-task=56
+#SBATCH --gpus-per-node=2
+#SBATCH --cpus-per-task=14
 #SBATCH --partition=dev-g
 
 
