f4e06f5a0a8779e227ca5402ee6e6e3a694e12d5
theodorross <theodor.a.ross@uit.no>
2025-03-11 15:46:44 +0100

    debugging on LUMI

--- gene_embedding/train_gene_autoencoder.py
+++ gene_embedding/train_gene_autoencoder.py
@@ -13,6 +13,7 @@ from utils import *
 print("tensorflow version:", tf.__version__)
 print("keras version:", keras.__version__, tf.keras.__version__)
 print("wandb version:", wandb.__version__)
+exit()
 
 
 def clip_gene(length):
