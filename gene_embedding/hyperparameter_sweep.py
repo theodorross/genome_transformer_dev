72b882fa1267692f0d539e2cd2ee47daab00a789
theodorross <theodor.a.ross@uit.no>
2025-03-14 14:52:07 +0100

    debugging hyperparameter sweep

--- gene_embedding/hyperparameter_sweep.py
+++ gene_embedding/hyperparameter_sweep.py
@@ -128,8 +128,8 @@ def train_gene_ae(config):
 
     ## Split the dataset into training and validation
     dataset_cuts = [gene_dataset.shard(5, k) for k in range(5)]
-    validation_genes = dataset_cuts[5]
-    training_genes = dataset_cuts[:5]
+    validation_genes = dataset_cuts[-1]
+    training_genes = dataset_cuts[:-1]
     training_genes = concatenate_datasets(*training_genes)
 
     ## Loop through the desired gene lengths
