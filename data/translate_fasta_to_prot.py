d7add4eca76628db4c89538853a6cece1838c98a
theodorross <theodor.a.ross@uit.no>
2025-03-18 11:51:01 +0100

    updating codebase

---
+++ data/translate_fasta_to_prot.py
@@ -0,0 +1,110 @@
+import os
+from tqdm import tqdm
+
+
+codontab = {
+    'TCA': 'S',    # Serina
+    'TCC': 'S',    # Serina
+    'TCG': 'S',    # Serina
+    'TCT': 'S',    # Serina
+    'TTC': 'F',    # Fenilalanina
+    'TTT': 'F',    # Fenilalanina
+    'TTA': 'L',    # Leucina
+    'TTG': 'L',    # Leucina
+    'TAC': 'Y',    # Tirosina
+    'TAT': 'Y',    # Tirosina
+    'TAA': '*',    # Stop
+    'TAG': '*',    # Stop
+    'TGC': 'C',    # Cisteina
+    'TGT': 'C',    # Cisteina
+    'TGA': '*',    # Stop
+    'TGG': 'W',    # Triptofano
+    'CTA': 'L',    # Leucina
+    'CTC': 'L',    # Leucina
+    'CTG': 'L',    # Leucina
+    'CTT': 'L',    # Leucina
+    'CCA': 'P',    # Prolina
+    'CCC': 'P',    # Prolina
+    'CCG': 'P',    # Prolina
+    'CCT': 'P',    # Prolina
+    'CAC': 'H',    # Histidina
+    'CAT': 'H',    # Histidina
+    'CAA': 'Q',    # Glutamina
+    'CAG': 'Q',    # Glutamina
+    'CGA': 'R',    # Arginina
+    'CGC': 'R',    # Arginina
+    'CGG': 'R',    # Arginina
+    'CGT': 'R',    # Arginina
+    'ATA': 'I',    # Isoleucina
+    'ATC': 'I',    # Isoleucina
+    'ATT': 'I',    # Isoleucina
+    'ATG': 'M',    # Methionina
+    'ACA': 'T',    # Treonina
+    'ACC': 'T',    # Treonina
+    'ACG': 'T',    # Treonina
+    'ACT': 'T',    # Treonina
+    'AAC': 'N',    # Asparagina
+    'AAT': 'N',    # Asparagina
+    'AAA': 'K',    # Lisina
+    'AAG': 'K',    # Lisina
+    'AGC': 'S',    # Serina
+    'AGT': 'S',    # Serina
+    'AGA': 'R',    # Arginina
+    'AGG': 'R',    # Arginina
+    'GTA': 'V',    # Valina
+    'GTC': 'V',    # Valina
+    'GTG': 'V',    # Valina
+    'GTT': 'V',    # Valina
+    'GCA': 'A',    # Alanina
+    'GCC': 'A',    # Alanina
+    'GCG': 'A',    # Alanina
+    'GCT': 'A',    # Alanina
+    'GAC': 'D',    # Acido Aspartico
+    'GAT': 'D',    # Acido Aspartico
+    'GAA': 'E',    # Acido Glutamico
+    'GAG': 'E',    # Acido Glutamico
+    'GGA': 'G',    # Glicina
+    'GGC': 'G',    # Glicina
+    'GGG': 'G',    # Glicina
+    'GGT': 'G'     # Glicina
+}
+
+
+def translate_gene(inp):
+    # n_codons = len(inp) // 3
+    codons = [inp[i:i+3] for i in range(0, len(inp), 3)]
+    # amino_acids = [codontab.get(_c,"-") for _c in codons]
+    amino_acids = [codontab.get(_c,"X") for _c in codons]
+    return "".join(amino_acids) + "\n"
+
+
+
+if __name__=="__main__":
+
+    # test = "ATCGTCGGATACGGCGATACGCT"
+    # translate_gene(test)
+
+    fasta_files = [f for f in os.listdir("gene_sequences") if "fasta" in f]
+    print(fasta_files)
+
+    for in_file in fasta_files:
+
+        ## define the output filename
+        basename = in_file.split(".")[0]
+        out_file = f"gene_sequences/prot_{basename}.faa"
+
+        ## Create an empty output file
+        with open(out_file, "w") as f_out:
+            f_out.write("")
+
+        ## Read each gene and translate it to amino acids
+        with open(f"gene_sequences/{in_file}","r") as f_in:
+            with open(out_file, "a") as f_out:
+
+                for line in tqdm(f_in.readlines()):
+                    if (">" in line) or (len(line)==1):
+                        f_out.write(line)
+                    else:
+                        f_out.write(translate_gene(line))
+
+
