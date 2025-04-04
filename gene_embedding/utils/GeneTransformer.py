0389b65b5106ccff7f32b32c183eb1bb648a39a9
theodorross <theodor.a.ross@uit.no>
2025-04-04 15:21:09 +0200

    debugging on LUMI

--- gene_embedding/utils/GeneTransformer.py
+++ gene_embedding/utils/GeneTransformer.py
@@ -101,7 +101,7 @@ class GeneTransformer(models.Model):
         # Define the objective function
         # self.reconstruction_loss = MaskedSparseCategoricalCrossentropy(mask_category=0)
         # self.triplet_loss = OnlineMarginTripletLoss(margin=5)
-        self.loss = "sparse_categorical_crossentropy"
+        self.reconstruction_loss = "sparse_categorical_crossentropy"
 
         # Define performance metrics to track
         levenshtein_metric = LevenshteinDistance(self.vocabulary)
