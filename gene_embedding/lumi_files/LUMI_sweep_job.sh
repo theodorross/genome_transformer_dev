e3bf7613a5d82b04a0e6714cd42867cf79bbd7a4
theodorross <theodor.a.ross@uit.no>
2025-04-30 13:07:20 +0200

    debugging cli sweep

--- gene_embedding/lumi_files/LUMI_sweep_job.sh
+++ gene_embedding/lumi_files/LUMI_sweep_job.sh
@@ -41,7 +41,7 @@ export SIF=/scratch/project_465001915/rosstheo/genome_transformer_dev/container/
 srun singularity exec \
     -B /scratch/project_465001915/rosstheo \
     $SIF /bin/bash \
-    -c '$WITH_CONDA && source wandb-env/bin/activate && python -m wandb sweep sweep_args.yml'
+    -c '$WITH_CONDA && source wandb-env/bin/activate && sh ~/wandb_login.sh && python -m wandb sweep sweep_args.yml'
     
 
 # ## Run the training script
--- gene_embedding/lumi_files/login_and_sweep.sh
+++ gene_embedding/lumi_files/login_and_sweep.sh
@@ -16,5 +16,5 @@ sh ~/wandb_login.sh
 # python hyperparameter_sweep.py
 
 ## Run the sweep
-python -m wandb agent $SWEEP_ID
+python -m wandb agent theodor-a-ross/$SWEEP_ID
 
