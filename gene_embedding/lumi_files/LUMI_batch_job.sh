cee75fffc112dc68596954d074e9ebd609cb9e1c
theodorross <theodor.a.ross@uit.no>
2025-05-14 12:33:49 +0200

    trying 1 GPUs again

--- gene_embedding/lumi_files/LUMI_batch_job.sh
+++ gene_embedding/lumi_files/LUMI_batch_job.sh
@@ -6,8 +6,8 @@
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gpus-per-node=2
-#SBATCH --cpus-per-task=14
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=7
 #SBATCH --mem=256G
 #SBATCH --partition=small-g
 
