eb3ddc3b90a5d8ccd02e02b9e93dfa4897c02068
theodorross <theodor.a.ross@uit.no>
2025-05-14 12:02:41 +0200

    trying 2 GPUs again

--- gene_embedding/lumi_files/LUMI_batch_job.sh
+++ gene_embedding/lumi_files/LUMI_batch_job.sh
@@ -6,9 +6,9 @@
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gpus-per-node=1
-#SBATCH --cpus-per-task=7
-#SBATCH --mem=128G
+#SBATCH --gpus-per-node=2
+#SBATCH --cpus-per-task=14
+#SBATCH --mem=256G
 #SBATCH --partition=small-g
 
 ## Load the needed LUMI bindings
