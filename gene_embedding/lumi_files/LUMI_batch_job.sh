9cd5eada45796c2ce3a0b6d4f4c4305a0aeb62b7
theodorross <theodor.a.ross@uit.no>
2025-05-16 12:07:19 +0200

    debugging GPU runs

--- gene_embedding/lumi_files/LUMI_batch_job.sh
+++ gene_embedding/lumi_files/LUMI_batch_job.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=7
-#SBATCH --mem-per-task=128G
+#SBATCH --mem=128G
 #SBATCH --partition=dev-g
 
 ## Load the needed LUMI bindings
