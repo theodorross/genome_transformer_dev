4fadf4c42aa7099b124dd8238fb2a9a70c6e96ee
theodorross <theodor.a.ross@uit.no>
2025-05-16 11:38:34 +0200

    debugging GPU runs

--- gene_embedding/lumi_files/LUMI_batch_job.sh
+++ gene_embedding/lumi_files/LUMI_batch_job.sh
@@ -3,12 +3,12 @@
 #SBATCH --output=lumi_files/logs/gene_embedding.o%j # Name of stdout output file
 #SBATCH --error=lumi_files/logs/gene_embedding.e%j  # Name of stderr error file
 #SBATCH --account=project_465001915
-#SBATCH --time=48:00:00
+#SBATCH --time=00:10:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=8
 #SBATCH --cpus-per-task=56
-#SBATCH --partition=standard-g
+#SBATCH --partition=dev-g
 
 ## Load the needed LUMI bindings
 module use /appl/local/containers/ai-modules
