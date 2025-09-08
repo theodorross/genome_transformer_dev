0cc8a05f5a5a8337c71b9e323fbc2a87d0edbb9a
theodorross <theodor.a.ross@uit.no>
2025-09-08 14:38:28 -0400

    changing wall time for embedding job

--- gene_embedding/lumi_files/LUMI_embedding_job.sh
+++ gene_embedding/lumi_files/LUMI_embedding_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=lumi_files/logs/gene_embedding.o%j # Name of stdout output file
 #SBATCH --error=lumi_files/logs/gene_embedding.e%j  # Name of stderr error file
 #SBATCH --account=project_465001915
-#SBATCH --time=72:00:00
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
