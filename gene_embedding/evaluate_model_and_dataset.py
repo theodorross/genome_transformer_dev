eea01eb0e379552438be391d6bc0c839e91610fb
theodorross <theodor.a.ross@uit.no>
2026-04-06 16:13:46 -0400

    updating

--- gene_embedding/evaluate_model_and_dataset.py
+++ gene_embedding/evaluate_model_and_dataset.py
@@ -5,6 +5,9 @@ from matplotlib import pyplot as plt
 import altair as alt
 import os
 import pandas as pd
+from tqdm import tqdm
+
+import time
 
 import argparse
 
@@ -15,11 +18,37 @@ def eval_cog_preds(model, dataset):
     Evaluate the COG category predictions of the gene autoencoder model.
     
     Inputs:
-        model ()
+        model (GeneTransformer) : keras model for the gene transformer model.
+        dataset (tensorflow.data.Dataset) : tensorflow dataset containing the genes, COG categories, and protein domains.
     '''
 
-    print(type(model))
-    print(type(dataset))
+    ## Initialize the lists to store data
+    genes = []
+    cog_categories = []
+
+    ## Populate the lists
+    for t in tqdm(dataset):
+        # dataset.append(g)
+        genes.append(t[0])
+        cog_categories.append(t[1])
+
+    for x in t:
+        print(x)
+    exit()
+
+    ## Compute the predictions
+    _geneseqs = tf.data.Dataset.from_tensor_slices(genes)
+    _,cog_preds,_ = model.predict(_geneseqs)
+
+    ## Define prediction and label arrays
+    cog_preds = (cog_preds > 0.5).astype(int)
+    correct_mask = np.all(cog_preds == cog_categories, axis=1)
+    print(f"genes completely correct:\t\t {correct_mask.sum():5d}\t ({correct_mask.mean()*100:3.2f}%)")
+
+    ## Find all unique combination of COG category
+    categs = np.asarray([c for c in 'ABCDEFGHIJKLMNOPQTUVWYZ'])
+    true_combo_vec, pred_combo_vec = [], []
+
 
 
     return
@@ -47,20 +76,32 @@ if __name__ == "__main__":
     Load the specified model and dataset
     '''
     ## Load the model
+    print("loading model...", end=" ")
+    t1 = time.time()
     gene_ae = tf.keras.models.load_model(args.model_path)
+    print(f"done. ({time.time()-t1:0.4f})")
     # gene_ae = None
 
     ## Load the dataset
+    print("loading dataset...", end=" ")
+    t1 = time.time()
     ds = tf.data.Dataset.load(args.dataset_path)
-    
-    # Filter the dataset to only keep genes up to 5000 nucleotides long
+    print(f"done. ({time.time()-t1:0.4f})")
+
+    # Filter the dataset to only keep genes up to 5000 nucleotides long and 
     gene_dataset = ds.filter(lambda g,d,c: tf.strings.length(g) <= 5000)
+
+    # Preprocess the dataset
+    print("preprocessing...", end=" ")
+    t1 = time.time()
+    gene_dataset = gene_ae.preprocess_dataset(gene_dataset, batch_size=4, weighted=False, shuffle=False)
+    print(f"done. ({time.time()-t1:0.4f})")
 
 
     '''
     Evalueate COG category predictions
     '''
-    eval_cog_preds(gene_ae, ds)
+    eval_cog_preds(gene_ae, gene_dataset)
 
 
 
